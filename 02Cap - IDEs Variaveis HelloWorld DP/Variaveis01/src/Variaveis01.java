bdf72497c768375cc284c101622e659ddb1082ae
Diogo Malfatti <diimalfatti@gmail.com>
2022-02-05 10:04:41 -0300

    upload 10 aulas

---
+++ 02Cap - IDEs Variaveis HelloWorld DP/Variaveis01/src/Variaveis01.java
@@ -0,0 +1,18 @@
+
+public class Variaveis01 {
+
+	public static void main(String[] args) {
+		
+		int idade = 10;
+		double preco = 10.5;
+		char sexo = 'M';
+		boolean temFilhos = true;
+		
+		System.out.println(idade);
+		System.out.println(preco);
+		System.out.println(sexo);
+		System.out.println(temFilhos);
+
+	}
+
+}
